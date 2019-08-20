870f7082f92fcb0e8948e6a45ed61f8f44636618
unknown <nicholas.gebhart@gmail.com>
2019-08-20 13:25:33 -0700

    Added closure solutions Need to complete one more stretch problem I need to look up how to add methods to this because I am not entirely sure and I want to read before I make an attempt instead of just writing code before I understand the problem

--- assignments/closure.js
+++ assignments/closure.js
@@ -4,22 +4,38 @@
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
 
+function parentFunction(para1) {
+  let var1 = "string";
+  let var2 = "string2";
+  console.log(`${var1} and ${var2}`);
+
+  function childFunction(para2) {
+    console.log(para2);
+
+    function grandFunction(para3) {
+      console.log(`get ${var1} even though I am in grandFunc`);
+    }
+    grandFunction("argument");
+  }
+  childFunction("argument2");
+}
+parentFunction("argument3");
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
-
 // ==== Challenge 2: Implement a "counter maker" function ====
 const counterMaker = () => {
-  // IMPLEMENTATION OF counterMaker:
-  // 1- Declare a `count` variable with a value of 0. We will be mutating it, so declare it using `let`!
-  // 2- Declare a function `counter`. It should increment and return `count`.
-  //      NOTE: This `counter` function, being nested inside `counterMaker`,
-  //      "closes over" the `count` variable. It can "see" it in the parent scope!
-  // 3- Return the `counter` function.
+  let count = 0;
+  function counter() {
+    count++;
+    return count;
+  }
+  return counter;
 };
-// Example usage: const myCounter = counterMaker();
-// myCounter(); // 1
-// myCounter(); // 2
+
+const myCounter = counterMaker();
+console.log(myCounter()); // 1
+console.log(myCounter()); // 2
 
 // ==== Challenge 3: Make `counterMaker` more sophisticated ====
 // It should have a `limit` parameter. Any counters we make with `counterMaker`
